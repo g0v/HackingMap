a6b5bbe13749412709259a2e95d89ad53c3ba5a4
choznerol <b02611002@ntu.edu.tw>
2017-11-12 11:57:05 +0800

    chore: Discard firebase config file & export auth service

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,15 +1,26 @@
-// Initialize firebase
 import Firebase from 'firebase/app'
 import 'firebase/database'
 import 'firebase/auth'
-import config from '../config/firebase.config.js'
 
-const FirebaseApp = Firebase.initializeApp(config)
+// Initialize Firebase
+const FirebaseApp = Firebase.initializeApp({
+  apiKey: 'AIzaSyBt4bk9EhaxpYaZygrEfRUPYUUuzgnnEiM',
+  authDomain: 'g0v-hackingmap.firebaseapp.com',
+  databaseURL: 'https://g0v-hackingmap.firebaseio.com',
+  projectId: 'g0v-hackingmap',
+  storageBucket: 'g0v-hackingmap.appspot.com',
+  messagingSenderId: '614284673952',
+})
 
 // Firebase Realtime Database
 const db = FirebaseApp.database()
 
 // Firebase Auth
-// TODO: add signInWithGoogle, signOut...
+const auth = FirebaseApp.auth()
+const signInOptions = [
+  Firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+]
 
-export { db }
+export { db, auth, signInOptions }
