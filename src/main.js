11c1f05b9baea3b13490d37b37641ea046bbd5a5
choznerol <b02611002@ntu.edu.tw>
2017-11-12 11:58:47 +0800

    chore(package): Add 'firebaseui'

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueFire from 'vuefire'
+import 'firebaseui/dist/firebaseui.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-TW'
